d534c2136d22c0914607a87b6831a4e20c9a70b8
Vikash <vikashyadav10648@gmail.com>
2025-08-28 16:21:34 +0530

    learned string literal template in js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let js = "amazing";
-const vikash = "chess player";
+const firstName = "Vikash";
+const birthYear = 2004;
+const year = 2025;
+const job = "chess player";
 console.log(40 + 8 + 23 - 10);
-console.log(vikash);
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + "years old" + job + "!";
+console.log(jonas);
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+
+console.log(`String
+  multiple
+  lines`);
+
+const age = 19;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("Sarah can start driving license");
+}
